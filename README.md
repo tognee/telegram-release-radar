a01082d26df53f28bb038de4e125ee9893eaa91d
tognee <AleXio64bit@gmail.com>
2019-07-06 17:17:31 +0200

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # telegram-release-radar
 Telegram bot that notifies you if a new track has been released on Spotify
+
+## Usage
+Rename `config_template.json` into `config.json` and add the Telegram bot Token and the Client ID and Secret from your Spotify App.
+`bot.py` will make the bot accept the commands to subscribe and unsubscribe from a specific artist.
+`releaseRadar.py` should be run whenever you want to check for new Albums and Singles.
+
+If you're on Windows you can use the Task Scheduler built into the OS
+If you're on Linux you can use crontab
+
+## Commands
+| command                             | function                                      |
+| ----------------------------------- | --------------------------------------------- |
+| `/start`                            | Gives a small introduction to the bot         |
+| spotify artist url or uri           | Subscribes or Unsubscribes to the artist link |
+| `.subs`                             | Lists all subscribed artists                  |
+| `.latest `spotify artist url or uri | Shows latest releases for that artist         |
---
+++ releaseRadar.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python3
+import telegram
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import datetime
+import sqlite3
+import time
+import os
+import json
+
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'config.json'), 'r') as f:
+    config = json.load(f)
+
+client_credentials_manager = SpotifyClientCredentials(client_id=config['spotifyClientID'],client_secret=config['spotifyClientSecret'])
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+TOKEN = config['botToken']
+bot = telegram.Bot(token=TOKEN)
+
+def escapeMarkdown(string):
+    string = str(string).replace("*","\*").replace("_","\_").replace("*","\*").replace("`","\`")
+    return string
+
+def chunks(l, n):
+	for i in range(0, len(l), n):
+		yield l[i:i+n]
+
+def dbSetup(conn, cur):
+	stmt1 = "CREATE TABLE IF NOT EXISTS user (userID integer, artistID text)"
+	stmt2 = "CREATE TABLE IF NOT EXISTS artist (artistID text PRIMARY KEY, lastSingleID text, lastSingleDate text, lastAlbumID text, lastAlbumDate text)"
+	cur.execute(stmt1)
+	cur.execute(stmt2)
+	conn.commit()
+
+def addArtistToUser(conn, cur, userID, artistID):
+	cur.execute("INSERT INTO user (userID, artistID) VALUES (?, ?)", (userID, artistID))
+	conn.commit()
+
+def removeArtistFromUser(conn, cur, userID, artistID):
+	cur.execute("DELETE FROM user WHERE userID = (?) AND artistID = (?);", (userID, artistID))
+	conn.commit()
+
+def getArtistsForUser(conn, cur, userID):
+	cur.execute("SELECT artistID FROM user WHERE userID = (?)", (userID, ))
+	res = cur.fetchall()
+	if res:
+		return res
+	else:
+		return []
+
+def getUsersForArtist(conn, cur, artistID):
+	cur.execute("SELECT userID FROM user WHERE artistID = (?)", (artistID, ))
+	res = cur.fetchall()
+	if res:
+		return res
+	else:
+		return []
+
+def updateLastArtistSingle(conn, cur, artistID, lastID, lastDate):
+	cur.execute("UPDATE artist SET lastSingleID = (?), lastSingleDate = (?) WHERE artistID = (?);", (lastID, lastDate, artistID))
+	conn.commit()
+
+def updateLastArtistAlbum(conn, cur, artistID, lastID, lastDate):
+	cur.execute("UPDATE artist SET lastAlbumID = (?), lastAlbumDate = (?) WHERE artistID = (?);", (lastID, lastDate, artistID))
+	conn.commit()
+
+def addArtist(conn, cur, artistID, lastSingleID, lastSingleDate, lastAlbumID, lastAlbumDate):
+	cur.execute("INSERT INTO artist (artistID, lastSingleID, lastSingleDate, lastAlbumID, lastAlbumDate) VALUES (?, ?, ?, ?, ?)", (artistID, lastSingleID, lastSingleDate, lastAlbumID, lastAlbumDate))
+	conn.commit()
+
+def getArtist(conn, cur, artistID):
+	cur.execute("SELECT * FROM artist WHERE artistID = (?)", (artistID, ))
+	res = cur.fetchone()
+	if res:
+		return res
+	else:
+		return []
+
+def getArtists(conn, cur):
+	cur.execute("SELECT artistID FROM artist")
+	res = cur.fetchall()
+	if res:
+		return res
+	else:
+		return []
+
+def removeArtist(conn, cur, artistID):
+	cur.execute("DELETE FROM artist WHERE artistID = (?);", (artistID, ))
+	conn.commit()
+
+def botAddArtist(con, cur, artistId):
+	artist = getArtist(con, cur, artistId)
+	if not artist:
+		latestAlbum = getNewestRelease(artistId, 'album', True)
+		latestSingle = getNewestRelease(artistId, 'single', True)
+		addArtist(con, cur, artistId, latestSingle['id'], latestSingle['release_date'], latestAlbum['id'], latestAlbum['release_date'])
+		print("Now artist "+artistId+" is being tracked")
+		return
+
+def botRemoveArtist(con, cur, artistId):
+	users = getUsersForArtist(con, cur, artistId)
+	if len(users)==0:
+		removeArtist(con, cur, artistId)
+		print("Now artist "+artistId+" is no longer tracked")
+		return
+
+def generateMessage(release):
+	message = "*"+escapeMarkdown(release['name'])+"*\n_"+escapeMarkdown(release['artists'][0]['name'])+"_\n\n🎵 "+release['release_date']+"\n▶️ #"+release['album_type'][:1].upper()+release['album_type'][1:]
+	return message
+
+def sendReleaseToUsers(con, cur, artistId, release):
+	users = getUsersForArtist(con, cur, artistId)
+	message = generateMessage(release)
+	imageID = None
+	for user in users:
+		if not imageID:
+			imageID = bot.sendPhoto(user[0], release['images'][0]['url'], message, parse_mode="Markdown", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Spotify Link", url="https://open.spotify.com/album/"+release['id'])]])).photo[0].file_id
+		else:
+			bot.sendPhoto(user[0], imageID, message, parse_mode="Markdown", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Spotify Link", url="https://open.spotify.com/album/"+release['id'])]]))
+		time.sleep(1)
+
+def updateNewReleases(con, cur, artistId):
+	latestAlbum = getNewestRelease(artistId, 'album')
+	latestSingle = getNewestRelease(artistId, 'single')
+	artist = getArtist(con, cur, artistId)
+	if not artist:
+		addArtist(con, cur, artistId, latestSingle['id'], latestSingle['release_date'], latestAlbum['id'], latestAlbum['release_date'])
+		return
+	if latestSingle['id'] != artist[1]:
+		dbDate = datetime.datetime.strptime(artist[2], '%Y-%m-%d')
+		newDate = datetime.datetime.strptime(latestSingle['release_date'], '%Y-%m-%d')
+		if dbDate <= newDate:
+			updateLastArtistSingle(con, cur, artistId, latestSingle['id'], latestSingle['release_date'])
+			print("New single found for "+artistId+": "+latestSingle['artists'][0]['name']+" - "+latestSingle['name'])
+			sendReleaseToUsers(con, cur, artistId, latestSingle)
+	if latestAlbum['id'] != artist[3]:
+		dbDate = datetime.datetime.strptime(artist[4], '%Y-%m-%d')
+		newDate = datetime.datetime.strptime(latestAlbum['release_date'], '%Y-%m-%d')
+		if dbDate <= newDate:
+			updateLastArtistAlbum(con, cur, artistId, latestAlbum['id'], latestAlbum['release_date'])
+			print("New album found for "+artistId+": "+latestAlbum['artists'][0]['name']+" - "+latestAlbum['name'])
+			sendReleaseToUsers(con, cur, artistId, latestAlbum)
+
+
+def getNewestRelease(artistId, album_type, local = False):
+	if local:
+		lastRelease = sp.artist_albums(artistId,album_type=album_type,country='IT',limit=1)['items']
+		if len(lastRelease)>0:
+			lastRelease = lastRelease[0]
+		else:
+			lastRelease = {'release_date': '1910-01-01', 'id': ''}
+		return lastRelease
+	lastReleaseNZ = sp.artist_albums(artistId,album_type=album_type,country='NZ',limit=1)['items']
+	if len(lastReleaseNZ)>0:
+		lastReleaseNZ = lastReleaseNZ[0]
+	else:
+		lastReleaseNZ = {'release_date': '1910-01-01', 'id': ''}
+	lastReleaseIT = sp.artist_albums(artistId,album_type=album_type,country='IT',limit=1)['items']
+	if len(lastReleaseIT)>0:
+		lastReleaseIT = lastReleaseIT[0]
+	else:
+		lastReleaseIT = {'release_date': '1910-01-01', 'id': ''}
+	if lastReleaseNZ:
+		lastReleaseNZ['release_date'] = lastReleaseNZ['release_date'][:10]
+		lastReleaseIT['release_date'] = lastReleaseIT['release_date'][:10]
+		if lastReleaseNZ['id'] == lastReleaseIT['id']:
+			return lastReleaseIT
+		lrNZDate = datetime.datetime.strptime(lastReleaseNZ['release_date'], '%Y-%m-%d')
+		lrITDate = datetime.datetime.strptime(lastReleaseIT['release_date'], '%Y-%m-%d')
+		if (lrNZDate > lrITDate):
+			return lastReleaseNZ
+		else:
+			return lastReleaseIT
+	else:
+		return lastReleaseIT
+
+def botGetLastArtistReleases(userID, artistId):
+	con = sqlite3.connect(os.path.join(os.path.dirname(os.path.realpath(__file__)),'database.db'))
+	cur = con.cursor()
+	dbSetup(con, cur)
+	artist = getArtist(con, cur, artistId)
+	if artist:
+		if artist[1] != "":
+			lastSingle = sp.album(artist[1])
+			bot.sendPhoto(userID, lastSingle['images'][0]['url'], generateMessage(lastSingle), parse_mode="Markdown", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Spotify Link", url="https://open.spotify.com/album/"+lastSingle['id'])]]))
+		else:
+			bot.sendMessage(userID, "No last single recorded")
+		time.sleep(1)
+		if artist[3] != "":
+			lastAlbum = sp.album(artist[3])
+			bot.sendPhoto(userID, lastAlbum['images'][0]['url'], generateMessage(lastAlbum), parse_mode="Markdown", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Spotify Link", url="https://open.spotify.com/album/"+lastAlbum['id'])]]))
+		else:
+			bot.sendMessage(userID, "No last album recorded")
+	else:
+		bot.sendMessage(userID, artistId+" is not tracked, you should add it before viewing the latest releases!")
+
+
+def getSubscriptions(userID):
+	con = sqlite3.connect(os.path.join(os.path.dirname(os.path.realpath(__file__)),'database.db'))
+	cur = con.cursor()
+	dbSetup(con, cur)
+	artists = getArtistsForUser(con, cur, userID)
+	con.close()
+	lista = []
+	for artist in artists:
+		artist = artist[0]
+		lista.append(artist)
+	if len(lista)>0:
+		listLista = list(chunks(lista, 20))
+		result = ""
+		for listObj in listLista:
+			spotiArtists = sp.artists(listObj)
+			for artist in spotiArtists['artists']:
+				result += "- ["+escapeMarkdown(artist['name'])+"](https://open.spotify.com/artist/"+artist['id']+")\n"
+		if result == "":
+			result = "_No One_"
+	else:
+		result = "_No One_"
+	bot.sendMessage(userID, "*Currently subscribed to:*\n"+result, parse_mode="Markdown", disable_web_page_preview=True)
+
+
+def addRemoveArtist(userID, artistID):
+	con = sqlite3.connect(os.path.join(os.path.dirname(os.path.realpath(__file__)),'database.db'))
+	cur = con.cursor()
+	dbSetup(con, cur)
+	spotiArtist = sp.artist(artistID)
+	artists = getArtistsForUser(con, cur, userID)
+	if (artistID, ) in artists:
+		removeArtistFromUser(con, cur, userID, artistID)
+		print("Removed artist "+artistID+" ("+spotiArtist['name']+") for "+str(userID))
+		bot.sendMessage(userID, "You've unsubscribed to "+spotiArtist['name']+".")
+		botRemoveArtist(con, cur, artistID)
+	else:
+		addArtistToUser(con, cur, userID, artistID)
+		print("Added artist "+artistID+" ("+spotiArtist['name']+") for "+str(userID))
+		bot.sendMessage(userID, "You've subscribed to "+spotiArtist['name']+".")
+		botAddArtist(con, cur, artistID)
+	con.close()
+
+if __name__ == "__main__":
+	con = sqlite3.connect(os.path.join(os.path.dirname(os.path.realpath(__file__)),'database.db'))
+	cur = con.cursor()
+	dbSetup(con, cur)
+	artists = getArtists(con, cur)
+	for artist in artists:
+		updateNewReleases(con, cur, artist[0])
+	con.close()
+
+# users(id, artist)
+# artists(id, lastSingleID, lastSingleDate, lastAlbumID, lastAlbumDate)
